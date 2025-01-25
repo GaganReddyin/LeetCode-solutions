b83e81655b899baabf168f4b2ac8f8e2d68412e5
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2025-01-25 15:23:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (87.07%) - LeetSync

---
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int min = 101;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 10 == nums[i]) {
+                min = Math.min(min, i);
+            }
+        }
+
+        return min == 101 ? -1 : min;
+    }
+}
+
+// TC: O(n), SC: O(1)
