e3cf6b8b3bf7644342f1aa8011a5a8761d095c4a
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2025-03-15 16:54:28 +0530

    Time: 179 ms (89.71%) | Memory: 28.7 MB (50.00%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        def check(cap):
+            count=taken=0
+            for x in nums:
+                if taken:
+                    taken=False
+                elif x<=cap:
+                    count+=1
+                    taken=True
+            return count>=k
+        l,r=min(nums),max(nums)
+        while l<=r:
+            mid=l+(r-l)//2
+            if check(mid):
+                res=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return res       
