f7e7d068ef3a17af0eaf48d3d0bf3fd9836fbc5c
GAGAN REDDY <99109231+GaganReddyin@users.noreply.github.com>
2025-08-15 14:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (8.89%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        double x = Math.log10(n) / Math.log10(4);
+        return x == (int)x;
+    }
+}
